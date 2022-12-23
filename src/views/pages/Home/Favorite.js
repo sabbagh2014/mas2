d8bc554194f322313e593e7f90f68aa30674b9bf
husamsabbagh2022 <110656965+sabbagh2014@users.noreply.github.com>
2022-12-23 08:27:20 +0300

    Update Favorite.js

--- src/views/pages/Home/Favorite.js
+++ src/views/pages/Home/Favorite.js
@@ -76,40 +76,8 @@ const useStyles = makeStyles(() => ({
 
 function Bids(props) {
   const classes = useStyles();
-  const settings = {
-    dots: true,
-      infinite: false,
-      speed: 500,
-      slidesToShow: 4,
-      slidesToScroll: 4,
-      initialSlide: 0,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3,
-            slidesToScroll: 3,
-            infinite: true,
-            dots: true
-          }
-        },
-        {
-          breakpoint: 600,
-          settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
-          }
-        },
-        {
-          breakpoint: 480,
-          settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
-          }
-        }
-      ]
-    };
+  const settings = {}
+    
   return (
     // <Page title="Marketplace | Fungy">
     <Box className={classes.LoginBox}>
