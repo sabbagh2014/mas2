8144a57ee355f0ed0df2a70bbe62350bd17546c2
husamsabbagh2022 <110656965+sabbagh2014@users.noreply.github.com>
2022-12-23 08:47:54 +0300

    Update Suspend.js

--- src/component/Suspend.js
+++ src/component/Suspend.js
@@ -47,8 +47,49 @@ const useStyles = makeStyles(() => ({
 
 export default function Login({ data }) {
   const settings = {
-   
-    };
+     dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: true,
+    centerMode: false,
+    centerPadding: "60px",
+    className: "recomended",
+    autoplay: false,
+    autoplaySpeed: 3000,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          centerMode: false,
+          centerPadding: "0",
+          autoplay: false,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          centerMode: false,
+          centerPadding: "0",
+          autoplay: false,
+        },
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+          centerPadding: "0",
+          autoplay: false,
+        },
+      },
+    ],
+  };
   const classes = useStyles();
   return (
     <Box className={classes.LoginBox}>
