419501ee7f5cc0139b8d99854fbf904c3672f47d
husamsabbagh2022 <110656965+sabbagh2014@users.noreply.github.com>
2022-12-23 09:01:07 +0300

    Update Favorite.js

--- src/views/pages/Home/Favorite.js
+++ src/views/pages/Home/Favorite.js
@@ -127,7 +127,7 @@ function Bids(props) {
 
       <Box className={classes.cardslider}>
         <Container maxWidth="lg">
-          <Carousel centerMode={true} centerSlidePercentage={25} numItemsPerView={4}>
+          <Carousel centerMode={true} centerSlidePercentage={20} numItemsPerView={2}>
             <Box>
               <Box display="flex" alignItems="center">
                 <figure >
