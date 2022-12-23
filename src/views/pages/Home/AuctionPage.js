56d8d611c9b5fd289b6f8991b2a7974d5b773a09
husamsabbagh2022 <110656965+sabbagh2014@users.noreply.github.com>
2022-12-23 08:37:22 +0300

    Update AuctionPage.js

--- src/views/pages/Home/AuctionPage.js
+++ src/views/pages/Home/AuctionPage.js
@@ -65,9 +65,44 @@ const AuctionPage = () => {
   const [isLoadingCreators, setIsLoadingCreators] = useState(false);
   const [isLoadingBundles, setIsBundlesLoading] = useState(false);
   const [isLoadingAuctions, setIsLaodingAuctions] = useState(false);
-  const settings = {};
-    
-
+  const settings = {dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: true,
+    className: "recomended",
+    autoplay: false,
+    autoplaySpeed: 3000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          centerMode: false,
+          centerPadding: "0",
+          autoplay: false,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false,
+          centerPadding: "20px",
+          autoplay: false,
+        },
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1,
+          centerMode: false,
+          centerPadding: "20px",
+          autoplay: false,
+        },
+      },
+    ],
+  };
   const auctionNftListHandler = async () => {
     setIsLaodingAuctions(true);
     await axios({
