b7647c68c9c0f681eb332c753a085ff29c0fcd25
husamsabbagh2022 <110656965+sabbagh2014@users.noreply.github.com>
2022-12-23 08:42:30 +0300

    Update Subscriptions.js

--- src/views/pages/Profile/Subscriptions.js
+++ src/views/pages/Profile/Subscriptions.js
@@ -65,8 +65,47 @@ export default function Subscriptions({
   const classes = useStyles();
 
   const settings = {
-    
-    };
+    dots: false,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    arrows: true,
+    centerMode: false,
+    autoplay: false,
+    autoplaySpeed: 3000,
+    infinite: false,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          centerMode: false,
+          centerPadding: "0",
+          autoplay: false,
+        },
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          centerMode: false,
+          centerPadding: "0",
+          autoplay: false,
+        },
+      },
+      {
+        breakpoint: 450,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          centerMode: false,
+          centerPadding: "0",
+          autoplay: false,
+        },
+      },
+    ],
+  };
 
   return (
     <div className={classes.subscriptionBox}>
