a752f6034e8809e7df58f087088e2e2bc611b971
husamsabbagh2022 <110656965+sabbagh2014@users.noreply.github.com>
2022-12-24 10:34:19 +0300

    Update AuctionPage.js

--- src/views/pages/Home/AuctionPage.js
+++ src/views/pages/Home/AuctionPage.js
@@ -161,7 +161,15 @@ const AuctionPage = () => {
             Featured Creators
           </Typography>
         </div>
-        
+            <Carousel infiniteLoop={true} centerMode={true} centerSlidePercentage={isMobile ? 80 : 25} numItemsPerView={4}>
+            {userListToDisplay.map((data, i) => {
+              return (
+                <UserDetailsCard key={i}
+                  data={data}
+                />
+              );
+            })}
+          </Carousel>          
       </Container>
 
       <Container maxWidth='100%' style={{ paddingBottom: "30px", marginTop: "50px", 
