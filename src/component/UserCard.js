16ab1d0426c285ec5b13345fa8584506526b3b1a
husamsabbagh2022 <110656965+sabbagh2014@users.noreply.github.com>
2022-12-26 16:54:00 +0300

    Update UserCard.js

--- src/component/UserCard.js
+++ src/component/UserCard.js
@@ -204,7 +204,7 @@ export default function UserDetailsCard(data) {
                   {userCardData.speciality}
                 </Typography>
               }
-            </Box>
+            
           </Box>
         </Box>
         
