ed0d192f091fd8e13b329dc1466176c43e155167
husamsabbagh2022 <110656965+sabbagh2014@users.noreply.github.com>
2022-12-28 10:57:12 +0300

    Update UserCard.js

--- src/component/UserCard.js
+++ src/component/UserCard.js
@@ -154,8 +154,8 @@ export default function UserDetailsCard(data) {
     <Card className={classes.cards}>
       <img
             style={{zIndex:'0', position: 'absolute', top: '50%', left: '50%', transform: 'translate(-50%, -50%)', margin: 'auto', height: '100%'}}
-            src={userCardData.profilePic >
-           onClick={() => {navigate("/user-profile")}}
+            src={userCardData.profilePic}
+           onClick={() => {navigate("/user-profile")}}>
             
               </img>
       <Box className={classes.cardContent}>
