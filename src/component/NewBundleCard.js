90571fcc7eddb03e94c7de6e324a4f76d87ed4d9
husamsabbagh2022 <110656965+sabbagh2014@users.noreply.github.com>
2023-01-01 14:00:47 +0300

    Update NewBundleCard.js

--- src/component/NewBundleCard.js
+++ src/component/NewBundleCard.js
@@ -218,8 +218,6 @@ export default function BundleCard({ data }) {
         }
         action={
           <IconButton aria-label="settings"> 
-          onClick={() => {
-              navigate("/user-profile/"+userName)
           <MoreVertIcon />
           </IconButton>
         }
