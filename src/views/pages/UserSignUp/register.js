fb5f22d2d55dfccb7869731352008036f946e95b
husam sabbagh <110656965+sabbagh2014@users.noreply.github.com>
2023-01-03 13:25:52 +0300

    Update register.js

--- src/views/pages/UserSignUp/register.js
+++ src/views/pages/UserSignUp/register.js
@@ -261,7 +261,7 @@ export default function SignUp() {
                 setphone(e);
                 setphonevalid(phone =="" || isValidPhoneNumber(e));
               }}
-              onBlur={()=>setphonevalid(phone =="" || isValidPhoneNumber())}
+              onBlur={()=>setphonevalid(phone =="" || isValidPhoneNumber(phone))}
               />
             </Box>
 
