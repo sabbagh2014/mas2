cf242215f6b05d1f2aa96c69e4250a81d3d962dc
husam sabbagh <110656965+sabbagh2014@users.noreply.github.com>
2023-01-08 17:29:57 +0300

    Update register.js

--- src/views/pages/UserSignUp/register.js
+++ src/views/pages/UserSignUp/register.js
@@ -243,27 +243,27 @@ export default function SignUp() {
                 onBlur={(e)=>setemailvalid(isValidEmail(e.target.value))}
               />
             </Box>
-            <Box>
-              <label className={classes.labelText}>
-                Phone number
-              </label>
-              <MuiTelInput 
-              defaultCountry="US"
-              disableFormatting 
-              required
-              error={!phonevalid}
-              helperText={!phonevalid && "Please enter valid phone number"}
-              value={phone}
-              className={classes.inputText}
-              variant="outlined"
-              type="tel"
-              onChange={(e) => {
-                setphone(e);
-                setphonevalid(phone =="" || isValidPhoneNumber(e));
-              }}
-              onBlur={()=>setphonevalid(phone =="" || isValidPhoneNumber(phone))}
-              />
-            </Box>
+            //<Box>
+              //<label className={classes.labelText}>
+                //Phone number
+              //</label>
+              //<MuiTelInput 
+              //defaultCountry="US"
+              //disableFormatting 
+              //required
+              //error={!phonevalid}
+              //helperText={!phonevalid && "Please enter valid phone number"}
+              //value={phone}
+              //className={classes.inputText}
+              //variant="outlined"
+              //type="tel"
+              //onChange={(e) => {
+                //setphone(e);
+                //setphonevalid(phone =="" || isValidPhoneNumber(e));
+              //}}
+              //onBlur={()=>setphonevalid(phone =="" || isValidPhoneNumber(phone))}
+              ///>
+            //</Box>
 
             <Box>
               <label className={classes.labelText}>Password</label>
